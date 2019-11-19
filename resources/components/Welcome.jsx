76162b5693fc893d0913511c3a319fce315aa973
Pepe Valdivia <jvaldivia@softweb.pe>
2019-11-19 12:17:33 -0500

    usando state, v10

--- resources/components/Welcome.jsx
+++ resources/components/Welcome.jsx
@@ -1,6 +1,26 @@
 class Welcome extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      message: 'Bienvenido Oe',
+      count: 0,
+    }
+  }
+
+  changeMessage(){
+    this.setState({
+      message: 'Gracias por suscribirse OE',
+      count: this.state.count + 1
+    })
+  }
+
   render() {
-    return <h1>welcome =)</h1>
+    return (
+      <div>
+        <h1>welcome =), {this.state.message}, {this.state.count}</h1>
+        <button onClick={() => this.changeMessage()}>Suscribirse</button>
+      </div>
+    )
   }
 }
 
